99ecd254d8b1a453a7b637596e8607deee528694
Ratanatroeun003 <rnatroeun@gmail.com>
2026-06-28 20:57:08 +0700

    vue-route

--- vuejs-app/src/App.vue
+++ vuejs-app/src/App.vue
@@ -1,10 +1,7 @@
 <script setup></script>
+
 <template>
-  <h1>You did it now!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to
-    read the documentation
-  </p>
+  <router-view></router-view>
 </template>
+
 <style scoped></style>
